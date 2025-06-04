da96bc3ecbd25e84b2ee1a15bbddfa88e7069bba
mumbimuthiga <veronicmuthiga@gmail.com>
2025-06-05 00:00:35 +0300

    program model

--- adminsection/models.py
+++ adminsection/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Program(models.Model):
+    name=models.CharField(max_length=100)
+    description=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    created_by=models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
--- adminsection/views.py
+++ adminsection/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from .models import Program
 
 # Create your views here.
 
 def dashboard(request):
     return render(request, 'admin/dashboard.html')
+
+def profile(request):
+    return render(request,'admin/profile.html')
+
+def programs(request):
+    programs=Program.objects.all()
+    return render(request,'admin/programs.html',{'programs':programs})
+
+def jobs(request):
+    return render(request,'admin/jobs.html')
+
+def skills(request):
+    return render(request,'admin/skills.html')
+
+def edit_profile(request):
+    return render(request,'admin/edit_profile.html')
