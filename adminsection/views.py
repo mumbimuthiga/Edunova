a813a8882e992728bd155c03d399426881fbbc60
mumbimuthiga <veronicmuthiga@gmail.com>
2025-06-06 16:53:42 +0300

    Jobs Listings

--- adminsection/views.py
+++ adminsection/views.py
@@ -1,5 +1,5 @@
+import requests
 from django.shortcuts import render
-
 from programs.models import Programs
 
 
@@ -14,13 +14,31 @@ def profile(request):
 
 def programs_view(request):
     programs=Programs.objects.all()
-    print (programs)
-    for program in programs:
-        print(program) 
+    # print (programs)
+    # for program in programs:
+    #     print(program) 
     return render(request,'admin/programs.html',{'programs':programs})
 
 def jobs(request):
-    return render(request,'admin/jobs.html')
+    url = "https://api.adzuna.com/v1/api/jobs/gb/search/1"
+    params = {
+        "app_id": "21777ecc", 
+        "app_key": "32485eabf03019b39cb8b202fb77367e",  
+        "what": "developer",   
+        "where": "london"      
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        data = response.json()
+        # print(data)  
+        jobs = data.get("results", [])
+    except requests.RequestException as e:
+        print("API error:", e)
+        jobs = []
+
+    return render(request, 'admin/jobs.html', {"jobs": jobs})
 
 def skills(request):
     return render(request,'admin/skills.html')
