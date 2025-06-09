67b47e61fde4d856b4b067df20b5124a550ff83d
mumbimuthiga <veronicmuthiga@gmail.com>
2025-06-09 21:29:41 +0300

    settings updated

--- edunova/settings.py
+++ edunova/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-ctvv&%%95vs_mmd+fsn3z&-kb6ccj_u_*2_i+$g#-a$0(vhjwh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['careeredunova.azurewebsites.net']
 
 
 # Application definition
